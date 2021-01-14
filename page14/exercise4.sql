744a427a8ae72cf08b18f7ab59b1fc7d3828a71a
Youthis321 <sutisna0695@gmail.com>
2021-01-14 17:23:22 +0700

    First commit

---
+++ page14/exercise4.sql
@@ -0,0 +1,14 @@
+/*
+dibawah "FROM purchases" tambahkan code untuk
+menambahkan baris dimana kolom "price" adalah NULL
+*/
+/*
+dibawah "FROM purchases" tambahkan code untuk
+menambahkan baris dimana kolom "price" adalah NULL
+*/
+
+SELECT *
+FROM purchases
+WHERE price IS NULL;
+SELECT *
+FROM purchases
